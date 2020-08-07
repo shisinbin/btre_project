8b9dc8b313a1846a30b19e1f18df581ce22e2f26
shisinbin <sb1664@gmail.com>
2020-08-07 16:49:14 +0100

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -154,3 +154,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='enthuzimuzzy00@gmail.com'
 EMAIL_HOST_PASSWORD='ButterUponBacon847'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
